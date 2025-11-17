2c9b7e3c5c1cfc2c5993937058ef960a62d5c8d4
Jdavid765 <david.canoduran@hotmail.com>
2025-11-17 19:53:06 +0100

    j'ai compris comment me déplacer regarder mon cahier mes notes

--- sources/start_game.c
+++ sources/start_game.c
@@ -6,7 +6,7 @@
 /*   By: david <david@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/16 10:57:53 by david             #+#    #+#             */
-/*   Updated: 2025/11/17 19:49:39 by david            ###   ########.fr       */
+/*   Updated: 2025/11/17 19:52:57 by david            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,4 +27,8 @@ int	ft_print_walk(int keycode, t_game *game)
 	ft_printf("Numbers of movements = %d\tavec la touche = %d\n", game->player.walk, keycode);
 	game->player.walk++;
 	return (0);
+}
+int	ft_check_keycode(int keycode, t_game *game)
+{
+	
 }
